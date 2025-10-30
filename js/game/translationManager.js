0ec9574e8239f2ab6ad6d01faa31c53ae6811e6a
ieeah <lbnfnc90@gmail.com>
2025-10-30 01:03:00 +0100

    wip: predisposizione localizzazione

---
+++ js/game/translationManager.js
@@ -0,0 +1,79 @@
+/**
+ * Translation Manager
+ * Gestisce le traduzioni dell'applicazione
+ */
+
+import translations from "../translations.js";
+
+class TranslationManager {
+  constructor() {
+    this.currentLocale = "it_IT";
+    this.translations = translations;
+  }
+
+  /**
+   * Ottiene la lingua corrente
+   * @returns {string} Codice della lingua corrente
+   */
+  getCurrentLocale() {
+    return this.currentLocale;
+  }
+
+  /**
+   * Imposta la lingua corrente
+   * @param {string} locale - Codice della lingua (es. "it_IT", "en_EN")
+   */
+  setLocale(locale) {
+    if (this.translations[locale]) {
+      this.currentLocale = locale;
+    }
+  }
+
+  /**
+   * Ottiene una traduzione usando una chiave dot-notation
+   * @param {string} key - Chiave della traduzione (es. "menu.start")
+   * @returns {string} Traduzione o chiave se non trovata
+   */
+  t(key) {
+    const keys = key.split(".");
+    let value = this.translations[this.currentLocale];
+
+    for (const k of keys) {
+      if (value && value[k] !== undefined) {
+        value = value[k];
+      } else {
+        return key; // Ritorna la chiave se traduzione non trovata
+      }
+    }
+
+    return value;
+  }
+
+  /**
+   * Ottiene tutte le lingue disponibili
+   * @returns {Array<{code: string, name: string}>} Array di lingue disponibili
+   */
+  getAvailableLocales() {
+    return Object.keys(this.translations).map((code) => ({
+      code,
+      name: this.getLocaleName(code),
+    }));
+  }
+
+  /**
+   * Ottiene il nome leggibile di una lingua
+   * @param {string} code - Codice della lingua
+   * @returns {string} Nome della lingua
+   */
+  getLocaleName(code) {
+    const names = {
+      it_IT: "Italiano",
+      en_EN: "English",
+      es_ES: "Español",
+    };
+    return names[code] || code;
+  }
+}
+
+// Export singleton instance
+export const translationManager = new TranslationManager();
--- js/translations.js
+++ js/translations.js
@@ -1,11 +1,239 @@
 const translations = {
   it_IT: {
-    options_header: {
-      great: "test",
+    // Titoli e header
+    header: {
+      startMessage: "Scegli una difficoltà ed inizia una nuova partita!",
+      newGame: "Weee Giochiamoo?",
+    },
+
+    // Menu e opzioni
+    menu: {
+      difficulty: "Difficoltà",
+      flags: "Flags",
+      remainingBombs: "Bombe rimanenti",
+      timer: "Timer",
+      revealedCells: "Celle Scoperte",
+      history: "Storico Partite",
+      language: "Lingua",
+    },
+
+    // Livelli di difficoltà
+    difficulty: {
+      beginner: "Principiante",
+      easy: "Facile",
+      medium: "Medio",
+      hard: "Difficile",
+      expert: "Pazzo",
+    },
+
+    // Pulsanti
+    buttons: {
+      start: "Inizia",
+      surrender: "Arrenditi",
+      help: "Aiuto",
+      audioOn: "Audio On",
+      audioOff: "Audio Off",
+      lightsOn: "Turn Lights On",
+      lightsOff: "Turn Lights Off",
+    },
+
+    // Messaggi modali
+    modal: {
+      gameOver: "Game Over! Hai cliccato su una bomba!",
+      youWin: "Grande! Hai vinto!\nFacciamo un'altra partita?",
+      surrenderConfirm: "Sicuro?",
+      surrenderFirst: "Per arrenderti devi prima fare almeno una mossa!",
+      continue: "Andiamo!",
+      surrender: "Sì, mi arrendo",
+    },
+
+    // Messaggi di aiuto
+    help: {
+      needToStart: "Devi iniziare la partita per poter utilizzare un aiuto!",
+      noLastBomb: "Non puoi usare gli aiuti per trovare l'ultima bomba, bel tentativo!",
+      alreadyUsed: "Hai già utilizzato un aiuto in questa partita!",
+    },
+
+    // Reazioni (verranno usate casualmente)
+    reactions: {
+      good: [
+        "Ottimo!",
+        "Bravo!",
+        "Continua così!",
+        "Fantastico!",
+        "Perfetto!",
+        "Grande!",
+        "Eccellente!",
+        "Sei un fenomeno!",
+      ],
+      bad: [
+        "Oh no!",
+        "Attenzione!",
+        "Ops!",
+        "Che peccato!",
+        "Riprova!",
+        "Quasi!",
+        "Mannaggia!",
+      ],
     },
   },
-  en_EN: {},
-  es_ES: {},
+
+  en_EN: {
+    // Titles and headers
+    header: {
+      startMessage: "Choose a difficulty and start a new game!",
+      newGame: "Let's Play!",
+    },
+
+    // Menu and options
+    menu: {
+      difficulty: "Difficulty",
+      flags: "Flags",
+      remainingBombs: "Remaining Bombs",
+      timer: "Timer",
+      revealedCells: "Revealed Cells",
+      history: "Match History",
+      language: "Language",
+    },
+
+    // Difficulty levels
+    difficulty: {
+      beginner: "Beginner",
+      easy: "Easy",
+      medium: "Medium",
+      hard: "Hard",
+      expert: "Expert",
+    },
+
+    // Buttons
+    buttons: {
+      start: "Start",
+      surrender: "Surrender",
+      help: "Help",
+      audioOn: "Audio On",
+      audioOff: "Audio Off",
+      lightsOn: "Turn Lights On",
+      lightsOff: "Turn Lights Off",
+    },
+
+    // Modal messages
+    modal: {
+      gameOver: "Game Over! You clicked on a bomb!",
+      youWin: "Awesome! You won!\nAnother game?",
+      surrenderConfirm: "Are you sure?",
+      surrenderFirst: "You need to make at least one move to surrender!",
+      continue: "Let's go!",
+      surrender: "Yes, I surrender",
+    },
+
+    // Help messages
+    help: {
+      needToStart: "You need to start the game to use a hint!",
+      noLastBomb: "You can't use hints to find the last bomb, nice try!",
+      alreadyUsed: "You've already used a hint in this game!",
+    },
+
+    // Reactions (will be used randomly)
+    reactions: {
+      good: [
+        "Great!",
+        "Nice!",
+        "Keep going!",
+        "Fantastic!",
+        "Perfect!",
+        "Awesome!",
+        "Excellent!",
+        "You're amazing!",
+      ],
+      bad: [
+        "Oh no!",
+        "Watch out!",
+        "Oops!",
+        "Too bad!",
+        "Try again!",
+        "Almost!",
+        "Darn!",
+      ],
+    },
+  },
+
+  es_ES: {
+    // Títulos y encabezados
+    header: {
+      startMessage: "¡Elige una dificultad y comienza un nuevo juego!",
+      newGame: "¡Juguemos!",
+    },
+
+    // Menú y opciones
+    menu: {
+      difficulty: "Dificultad",
+      flags: "Banderas",
+      remainingBombs: "Bombas Restantes",
+      timer: "Temporizador",
+      revealedCells: "Celdas Reveladas",
+      history: "Historial de Partidas",
+      language: "Idioma",
+    },
+
+    // Niveles de dificultad
+    difficulty: {
+      beginner: "Principiante",
+      easy: "Fácil",
+      medium: "Medio",
+      hard: "Difícil",
+      expert: "Experto",
+    },
+
+    // Botones
+    buttons: {
+      start: "Iniciar",
+      surrender: "Rendirse",
+      help: "Ayuda",
+      audioOn: "Audio On",
+      audioOff: "Audio Off",
+      lightsOn: "Encender Luces",
+      lightsOff: "Apagar Luces",
+    },
+
+    // Mensajes modales
+    modal: {
+      gameOver: "¡Game Over! ¡Has clickeado una bomba!",
+      youWin: "¡Genial! ¡Has ganado!\n¿Otra partida?",
+      surrenderConfirm: "¿Estás seguro?",
+      surrenderFirst: "¡Necesitas hacer al menos un movimiento para rendirte!",
+      continue: "¡Vamos!",
+      surrender: "Sí, me rindo",
+    },
+
+    // Mensajes de ayuda
+    help: {
+      needToStart: "¡Necesitas iniciar el juego para usar una pista!",
+      noLastBomb: "¡No puedes usar pistas para encontrar la última bomba, buen intento!",
+      alreadyUsed: "¡Ya has usado una pista en este juego!",
+    },
+
+    // Reacciones (se usarán aleatoriamente)
+    reactions: {
+      good: [
+        "¡Genial!",
+        "¡Bien!",
+        "¡Sigue así!",
+        "¡Fantástico!",
+        "¡Perfecto!",
+        "¡Excelente!",
+        "¡Eres increíble!",
+      ],
+      bad: [
+        "¡Oh no!",
+        "¡Cuidado!",
+        "¡Ups!",
+        "¡Qué pena!",
+        "¡Inténtalo de nuevo!",
+        "¡Casi!",
+        "¡Vaya!",
+      ],
+    },
+  },
 };
 
 export default translations;
